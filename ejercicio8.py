92e1eaaff36dfde3252be680ddaf218c68084bfa
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-02-18 23:21:45 +0100

    Completado código ejercicio 8

--- ejercicio8.py
+++ ejercicio8.py
@@ -25,7 +25,7 @@ Realización
     si n >= 4
         p += 200.00 + 20.00 * (n-4)
 
-    si damage < 0.20 * p:
+    si damage < 0.20 * p
         nada
     si no
         si a = 1
@@ -42,3 +42,33 @@ Poscondición
 
 fin prima
 """
+
+def prima(d: int, n: int, a: int, damage: float):
+    if a < 0 or d < 0 or n < 0:
+        raise ValueError("a, d and n must be greater than or equal to cero")
+    if damage < 0:
+        raise ValueError("Damage cannot drop below cero.")
+
+    p = 0
+
+    if d >= 40_000:
+        p += 400.00
+    else:
+        p += 0.01 * d
+
+    if n >= 4:
+        p += 200.00 + 20.00 * (n-4)
+
+    if damage < 0.20 * p:
+        pass
+    else:
+        if a == 1:
+            p /= 2
+        elif a == 2:
+            p /= 3
+        elif a == 3:
+            p /= 4
+        else:
+            p = 0
+
+    return p
