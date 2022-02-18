9052a3aac8481ef6d17c7bf6daed23bb06a7714c
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-02-18 23:08:47 +0100

    Creación del algoritmo del ejercicio 8

---
+++ ejercicio8.py
@@ -0,0 +1,19 @@
+""" --- Algoritmo - Consulta ---
+Algoritmo prima
+
+Entrada:
+    d: ENTERO  # Distancia recorrida en kilometros
+    n: ENTERO  # Años de antigüedad
+
+Salida: DECIMAL
+
+Variables
+    p: DECIMAL <-- 0  # Prima
+
+Realización
+    
+
+
+
+
+"""
