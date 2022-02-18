e74fe03f543d931620573f403700b3abe5119745
AlejandroIlgesias <93452718+AlejandroIlgesias@users.noreply.github.com>
2022-02-18 23:01:21 +0100

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,10 @@
+#Ejercicio 2:
+#1)entrada:
+    #a:numero real
+    #b:numero real
+#2)realizar la suma de a y b:a+b
+#3)realizar el producto de a y b: axb
+#4)comparar primero los numeros a y b,
+#5)Comparar el producto con uno de ellos y la suma con ese mismo numero,
+#(por ejemplo,comparar axb con a y comparar a+b con a)
+#6)Por último,ordenar los resultados de menor a mayor.
