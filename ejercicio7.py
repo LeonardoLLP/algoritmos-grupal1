b3f6b6cfff925d96b3e1c7e45d7f5018cb6f80a2
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-02-18 22:54:24 +0100

    Escrito código algoritmo 7

--- ejercicio7.py
+++ ejercicio7.py
@@ -9,7 +9,7 @@ Entrada:
 Salida: DECIMAL
 
 Precondición
-    n > 0
+    n, d > 0
     para = "todos" OR para = "uno"
 
 Variables
@@ -18,7 +18,7 @@ Variables
 Realización
     c += 3.50 * d
 
-    si n <= 25:
+    si n <= 25
         c += 67.30
     si no
         c += 61.00
@@ -38,3 +38,29 @@ Poscondición
 
 fin coste
 """
+
+def coste(n: int, d: int, para: str):
+    if n <= 0 or d <= 0:
+        raise ValueError("Días y número de alumnos tienen que ser positivos.")
+    elif para not in ["todos", "uno"]:
+        raise ValueError('La variable "para" solamente puede valer "todos" o "uno".')
+    c = 0
+
+    c += 3.50 * d
+
+    if n <= 25:
+        c += 67.30
+    else:
+        c += 61.00
+
+    if n <= 30:
+        c += 4.75 * d
+    elif n <= 35:
+        c += 4.00 * d
+    else:
+        c += 3.50 * d
+
+    if para == "todos":
+        return c * d
+    else:
+        return c
