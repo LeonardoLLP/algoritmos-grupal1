b83d6254c74e8a66c990e711a71d68470f71f8c2
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-02-18 23:15:55 +0100

    Creado algoritmo ejercicio 8

--- ejercicio8.py
+++ ejercicio8.py
@@ -4,6 +4,8 @@ Algoritmo prima
 Entrada:
     d: ENTERO  # Distancia recorrida en kilometros
     n: ENTERO  # Años de antigüedad
+    a: ENTERO  # Accidentes
+    damage: DECIMAL  # Dinero invertido en accidentes
 
 Salida: DECIMAL
 
@@ -11,9 +13,28 @@ Variables
     p: DECIMAL <-- 0  # Prima
 
 Realización
-    
+    si d >= 40_000
+        p += 400.00
+    si no
+        p += 0.01 * d
 
+    si n >= 4
+        p += 200.00 + 20.00 * (n-4)
 
+    si damage < 0.20 * p:
+        nada
+    si no
+        si a = 1
+            p <-- p/2
+        si no si a = 2
+            p <-- p/3
+        si no si a = 3
+            p <-- p/4
+        si no
+            p = 0
 
+Poscondición
+    Resultado = p
 
+fin prima
 """
